2f1073cc4dd6910e1364d6db7e5edc560723605c
Sunny <mahmud.moind.1@gmail.com>
2023-08-17 19:19:02 +0600

    sets  variable to set site title

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -1,3 +1,6 @@
+<?php
+if(!isset($page_title)) {$page_title = 'Staff Area'; }
+?>
 <!doctype html>
 <html lang = "en" >
 <head >
@@ -5,7 +8,7 @@
              <meta name = "viewport"
                    content = "width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" >
                          <meta http-equiv = "X-UA-Compatible" content = "ie=edge" >
-             <title >GBI</title >
+	<title >GBI - <?php echo $page_title ;?> </title >
 	<link rel = "stylesheet" href = "../stylesheets/staff.css" >
 </head >
 <body >
